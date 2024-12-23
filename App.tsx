694c0215fe4a0bcdb9611297b23b90c80d9dfc47
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-12-23 19:30:30 +0530

    defined states+utility functions

--- App.tsx
+++ App.tsx
@@ -1,7 +1,13 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () : React.JSX.Element => {
+	const [password, setPassword] = useState<string>('');
+	const [isPasswordGenerated, setIsPasswordGenerated] = useState<boolean>(false);
+	const [lowercase, setLowercase] = useState<boolean>(true);
+	const [uppercase, setUppercase] = useState<boolean>(true);
+	const [setsymbols, setSetsymbols] = useState<boolean>(true);
+	
 	return (
 		<View>
 			<Text>App</Text>
---
+++ _utils/utils.ts
@@ -0,0 +1,7 @@
+export const generatePasswordStr = (passwordLength: number) => {
+
+};
+
+export const createPassword = (characters: string) => {
+    
+}
