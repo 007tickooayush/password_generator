06286e3280b8d25ba6fd1b5e6cbaae998a966bae
Ayush Tickoo <ayush.tickoo292000@gmail.com>
2024-12-24 17:55:10 +0530

    initialized basic form format+added basic form styles+added react-native Formik components

--- App.tsx
+++ App.tsx
@@ -1,29 +1,81 @@
-import { View, Text, SafeAreaView, ScrollView, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView, TouchableOpacity, TextInput, Button } from 'react-native'
 import React, { useState } from 'react'
 import { styles } from './_utils/styles';
-import { getSchemeBasedColor } from './_utils/utils';
+import { generatePasswordStr, getSchemeBasedColor } from './_utils/utils';
+import { Form, Formik } from 'formik';
+import { PasswordSchema } from './_utils/schema';
 
-const App = () : React.JSX.Element => {
+const App = (): React.JSX.Element => {
 	const [password, setPassword] = useState<string>('');
 	const [isPasswordGenerated, setIsPasswordGenerated] = useState<boolean>(false);
 	const [lowercase, setLowercase] = useState<boolean>(true);
 	const [uppercase, setUppercase] = useState<boolean>(true);
-	const [setsymbols, setSetsymbols] = useState<boolean>(true);
-	
+	const [numbers, setNumbers] = useState<boolean>(true);
+	const [symbols, setSymbols] = useState<boolean>(true);
+
 	return (
 		<SafeAreaView>
 			<ScrollView>
 				<View style={[styles.container]}>
 					<View style={[styles.header]}>
 						<Text style={[styles.headingText]}>
-							App Title
+							Password Generator
 						</Text>
 					</View>
-					<TouchableOpacity style={[styles.button]}>
-						<Text style={[styles.buttonText]}>
-							Generate Password
-						</Text>
-					</TouchableOpacity>
+					<Formik
+						initialValues={{ passwordLength: '' }}
+						onSubmit={(values) => {
+							console.log(values);
+							let result = generatePasswordStr(+values.passwordLength, uppercase, lowercase, numbers, symbols);
+							console.log('result :>> ', result);
+						}}
+						validationSchema={PasswordSchema}
+					>
+						{({ 
+							handleChange,
+							handleBlur,
+							handleSubmit,
+							errors,
+							touched,
+							isValid,
+							handleReset,
+							values 
+						}) => (
+							<View style={styles.formContainer}>
+								<View style={styles.inputContainer}>
+									<View style={styles.inputHolder}>
+										<TextInput 
+											style={[styles.inputBar, styles.normalText]}
+											value={values.passwordLength}
+											onChangeText={handleChange('passwordLength')}
+											placeholder='Enter password length...'
+											keyboardType='numeric'
+										/>
+									</View>
+									<View style={styles.inputHolder}></View>
+									<View style={styles.inputHolder}></View>
+									<View style={styles.inputHolder}></View>
+									<View style={styles.inputHolder}></View>
+								</View>
+
+								<View style={styles.actionsContainer}>
+									<TouchableOpacity style={[styles.button]}>
+										<Text style={[styles.buttonText]}>
+											Generate Password
+										</Text>
+									</TouchableOpacity>
+									<TouchableOpacity style={[styles.button]}>
+										<Text style={[styles.buttonText]}>
+											Reset Password
+										</Text>
+									</TouchableOpacity>
+								</View>
+
+
+							</View>
+						)}
+
+					</Formik>
 				</View>
 			</ScrollView>
 		</SafeAreaView>
--- _utils/styles.ts
+++ _utils/styles.ts
@@ -31,9 +31,43 @@ export const styles = StyleSheet.create({
         backgroundColor: color,
         padding: 10,
         borderRadius: 5,
+        margin: 4,
     },
     buttonText: {
         color: getSchemeBasedColor(true),
         fontSize: 16,
+    },
+    formContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+    },
+    inputBar: {
+        borderWidth: 1,
+        borderColor: color,
+        borderRadius: 5,
+        width: 400,
+        padding: 4,
+    },
+    inputContainer: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        margin: 2
+    },
+    inputHolder: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 4,
+    },
+    actionsContainer: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        padding: 4,
+        margin: 4,
     },
 });
